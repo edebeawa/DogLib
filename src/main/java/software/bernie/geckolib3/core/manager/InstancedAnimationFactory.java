bba1345dc82f8f9c69ce8cc35f3b4340b0dec17d
edebe <3614332960@qq.com>
2023-07-01 04:40:54 +0800

    Initial commit

---
+++ src/main/java/software/bernie/geckolib3/core/manager/InstancedAnimationFactory.java
@@ -0,0 +1,26 @@
+package software.bernie.geckolib3.core.manager;
+
+import software.bernie.geckolib3.core.IAnimatable;
+
+/**
+ * AnimationFactory implementation for instantiated objects such as Entities or BlockEntities. Returns a single {@link AnimationData} instance per factory.
+ */
+public class InstancedAnimationFactory extends AnimationFactory {
+	private AnimationData animationData;
+
+	@SuppressWarnings("removal")
+	public InstancedAnimationFactory(IAnimatable animatable) {
+		super(animatable);
+	}
+
+	@Override
+	public AnimationData getOrCreateAnimationData(int uniqueID) {
+		if (this.animationData == null) {
+			this.animationData = new AnimationData();
+
+			this.animatable.registerControllers(this.animationData);
+		}
+
+		return this.animationData;
+	}
+}
